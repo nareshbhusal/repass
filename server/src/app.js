d7925ea3d092f2a433faa507885679c4b84418f5
nareshsharma123 <nareshsharma123@gmail.com>
2019-04-06 11:46:34 +0530

    Start database

--- server/src/app.js
+++ server/src/app.js
@@ -3,4 +3,24 @@ const hbs = require('hbs');
 const path = require('path');
 const db = require('./config/database');
 
-// Session 
+const app = express();
+
+// Configure middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Initialise database
+
+//Set routes
+app.get('', (req, res) => {
+    const data = [
+        {name: 'Naresh', age: 19},
+        {name: 'Ariadne', age: 19}
+    ]
+    res.send(data);
+})
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log(`listening at port ${port}`);
+})
