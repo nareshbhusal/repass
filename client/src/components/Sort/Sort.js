2cd1b4a0e98f595f367057e65cb3f8a810b25d4c
nareshsharma123 <nareshsharma123@gmail.com>
2019-04-07 21:00:08 +0530

    Add styling to sort component

--- client/src/components/Sort/Sort.js
+++ client/src/components/Sort/Sort.js
@@ -2,10 +2,23 @@ import React from 'react';
 import styles from './Sort.module.css';
 
 class Sort extends React.Component{
+    onChange = (e) => {
+        const value = e.target.options[e.target.selectedIndex].value;
+        console.log(value);
+        // 
+    }
     render(){
         return (
             <div className={styles.sort}>
-                sort
+                Sort
+                <select onChange={this.onChange}>
+                    <option value="top">
+                        Top
+                    </option>
+                    <option value="new">
+                        Newest
+                    </option>
+                </select>
             </div>
         )
     }
