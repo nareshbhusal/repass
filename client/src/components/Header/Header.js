3a6bd2037a44c58a78d3634768d20c525fcd056d
nareshsharma123 <nareshsharma123@gmail.com>
2019-04-07 12:06:49 +0530

    Add styling to header

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -5,48 +5,65 @@ import { Link } from 'react-router-dom';
 class Header extends React.Component{
 
     state = {
-        theme: 'light'
+        theme: 'light',
+        loggedIn: false
     }
+
+    changeTheme = () => {
+        const newTheme = this.state.theme === 'light' ? 'dark' : 'light';
+        this.setState({ theme:  newTheme});
+    }
+
     renderThemeBtn = () => {
-        if (this.state.theme ==='light') {
-            return (
-                <div className={styles.themeBtn}>
-                    <i className="fa fa-moon-o"></i>
-                </div>
-            )
-        } else {
-            return (
-                <div className={styles.themeBtn}>
+        return (
+            <button onClick={this.changeTheme} className={styles.themeBtn}>
+                {
+                    this.state.theme === 'light' ?
+                    <i className="fa fa-moon-o"></i> :
                     <i className="fa fa-sun-o"></i>
+                }
+                
+            </button>
+        );
+    }
+    renderSignInBtns = () => {
+        if (!this.state.loggedIn) {
+            return (
+                <div className={styles.signInBtns}>
+                    <Link className={`${styles.btn} ${styles.btnLogin}`} to="/login">
+                        Login
+                    </Link>
+                    <Link className={`${styles.btn} ${styles.btnSignup}`} to="/register">
+                        Signup
+                    </Link>
                 </div>
             )
-        }
-    }
-    renderActions = () => {
-        if (!this.state.loggedIn) {
-            <div className={styles.signinBtns}>
-                <Link className={`${styles.btn} ${styles.btnLogin}`} to="/login">
-                    Login
-                </Link>
-                <Link className={`${styles.btn} ${styles.btnSignup}`} to="/register">
-                    Signup
-                </Link>
-            </div>
+            
         } else {
-            <div className={styles.user}>
-                <i className="fa fa-user"></i>
-                user
-            </div>
+            return (
+                <div className={styles.userCtrl}>
+                    <Link className={styles.user}>
+                        <i className="fa fa-user"></i>
+                        username
+                    </Link>
+                    <Link className={`${styles.btn} ${styles.btnLogout}`} to="/login">
+                        <i className="fa fa-sign-out"></i>Logout
+                    </Link>
+                </div>
+            );
         }
     }
     render() {
+        console.log(this.state);
         return (
             <div className={styles.header}>
                 <Link className={styles.logo} to="/">
-                    Repass
+                    repass
                 </Link>
-                {this.renderThemeBtn()}
-                {this.renderActions()}
+                <div className={styles.actions}>
+                    {this.renderThemeBtn()}
+                    {this.renderSignInBtns()}
+                </div>
             </div>
         )
     }
