572069431d54c0ef25d1ff69b739094c9d23e52a
nareshsharma123 <nareshsharma123@gmail.com>
2019-06-15 13:23:17 +0530

    Add regsiter route handler

--- server/src/routes/users/index.js
+++ server/src/routes/users/index.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express.Router();
 const getUsers = require('./getUsers');
 const getUser = require('./getUser');
+const register = require('./register');
 
-router.use('/users/', getUsers);
+router.get('/users/', getUsers);
+router.get('/users/register', register);
 router.use('/u/:username', getUser);
 
 module.exports = router;
---
+++ server/src/routes/users/register.js
@@ -0,0 +1,33 @@
+const User = require('../../models/User');
+
+const registerUser = async(req, res) => {
+    // const { username, email, password } = req.body;
+    const { username, email, password } = req.query;
+    // server side validation
+    const errors = [];
+    if (!username || !password || !email) {
+        errors.push({ err: 'Please fill in all fields' });
+        return res.send(errors);
+    }
+
+    // Check if the user already exists
+    const userInRecords = await User.findOne({
+        where: {
+            username,
+        }
+    });
+    if (userInRecords) {
+        errors.push({ err: 'Username taken!' })
+        return res.send(errors);
+    }
+    // create the user
+    const user = await User.create({
+        username,
+        password,
+        email
+    });
+
+    return res.send(user);
+}
+
+module.exports = registerUser;
