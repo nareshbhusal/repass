5bf6384eabb0e56e32638083d9274a026983c504
nareshsharma123 <nareshsharma123@gmail.com>
2019-06-17 17:11:32 +0530

    Add getVote

---
+++ server/src/controllers/Listing/getVote.js
@@ -0,0 +1,24 @@
+const updateListing = require('./updateListing');
+const Listing = require('../../models/Listing');
+
+const getVote = async(listingId, username) => {
+    const listing = await Listing.findOne({
+        where: {
+            id: listingId
+        }
+    });
+    const downvotesList = listing.downs;
+    const upvotesList = listing.ups;
+    const inDownvotesList = downvotesList.indexOf(username) === -1;
+    const inUpvotesList = upvotesList.indexOf(username) === -1;
+
+    if (inDownvotesList) {
+        return 0;
+    } else if (inUpvotesList) {
+        return 1;
+    } else {
+        return null;
+    }
+}
+
+module.exports = getVote;
