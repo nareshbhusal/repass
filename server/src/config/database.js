5d78a2bd374a3478f72b991e0ed9d8b5693bc466
nareshsharma123 <nareshsharma123@gmail.com>
2019-09-29 14:31:33 +0530

    Fix env variabless

--- server/src/config/database.js
+++ server/src/config/database.js
@@ -1,7 +1,6 @@
 const Sequelize = require('sequelize');
 const { DB_NAME, DB_USER, DB_PASS, HOST } = process.env;
 
-console.log(DB_NAME, DB_PASS, DB_USER)
 module.exports = new Sequelize(DB_NAME, DB_USER, DB_PASS, {
     host: HOST,
     dialect: 'postgres',
